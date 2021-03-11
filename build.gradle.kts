4d1d58949d9d5e262d69c3784341ae598194e063
Jonathan Bisson <bjo@uic.edu>
2021-03-11 12:29:06 -0600

    Move to modular versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,27 @@
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+
 plugins {
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm")
     application
-    id("org.beryx.jlink") version "2.23.1"
+    id("org.beryx.jlink")
+    id("com.github.ben-manes.versions")
+    id("io.gitlab.arturbosch.detekt")
+    id("org.jlleitschuh.gradle.ktlint")
+    id("org.jmailen.kotlinter")
+    //id("org.openjfx.javafxplugin")  // This doesn't work…
 }
 
 val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
 val compileJava: JavaCompile by tasks
 compileJava.destinationDir = compileKotlin.destinationDir
 
+val publicationName: String by project
+group = "net.nprod"
+version = "0.0.1" + if (System.getProperty("snapshot")?.isEmpty() != false) {
+    ""
+} else {
+    "-SNAPSHOT"
+}
 
 repositories {
     mavenCentral()
@@ -24,29 +38,40 @@ sourceSets.main {
 }
 
 java {
-   modularity.inferModulePath.set(true)
+    modularity.inferModulePath.set(true)
 }
 
 dependencies {
-    val coroutinesVersion = "1.4.2"
+    val coroutinesVersion: String by project
+    val sqliteVersion: String by project
+    val apacheCommonsTextVersion: String by project
+    val cliktVersion: String by project
+    val javafxVersion: String by project
+
 
     implementation(kotlin("stdlib"))
-    implementation("org.xerial:sqlite-jdbc:3.32.3.2")
-    implementation("org.apache.commons:commons-text:1.9")
-    implementation("com.github.ajalt.clikt:clikt:3.1.0") {
+    implementation("org.xerial:sqlite-jdbc:$sqliteVersion")
+    implementation("org.apache.commons:commons-text:$apacheCommonsTextVersion")
+    implementation("com.github.ajalt.clikt:clikt:$cliktVersion") {
         exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
     }
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:$coroutinesVersion")
 
-    val platform = "linux"
-    implementation("org.openjfx:javafx-base:15.0.1:${platform}")
-    implementation("org.openjfx:javafx-controls:15.0.1:${platform}")
-    implementation("org.openjfx:javafx-graphics:15.0.1:${platform}")
-    implementation("org.openjfx:javafx-web:15.0.1:${platform}")
-    implementation("org.openjfx:javafx-media:15.0.1:${platform}")
-    implementation("org.openjfx:javafx-fxml:15.0.1:${platform}")
+    val os: OperatingSystem = DefaultNativePlatform.getCurrentOperatingSystem()
+    val platform =
+        if (os.isLinux) "linux" else if (os.isWindows) "windows" else throw RuntimeException(
+            "Your OS isn't managed yet. To port it, you will need to have the bruker library compiled\n" +
+                    " for your OS and your platform, so you will have to contact them. Good luck with that.")
+
+    implementation("org.openjfx:javafx-base:$javafxVersion:$platform")
+    implementation("org.openjfx:javafx-controls:$javafxVersion:$platform")
+    implementation("org.openjfx:javafx-graphics:$javafxVersion:$platform")
+    implementation("org.openjfx:javafx-web:$javafxVersion:$platform")
+    implementation("org.openjfx:javafx-media:$javafxVersion:$platform")
+    implementation("org.openjfx:javafx-fxml:$javafxVersion:$platform")
+
 }
 /*
 distributions {
@@ -72,14 +97,14 @@ jlink {
     //addExtraDependencies("com.github.ajalt.clikt")
     imageZip.set(file("$buildDir/kbaf2mzml-$version.zip"))
     options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
-    targetPlatform("windows-x64") {
+    /*targetPlatform("windows-x64") {
         setJdkHome("stuff/jdk-15.0.2+7")
         extraModulePaths.add("stuff/javafx-jmods-15.0.1")
     }
     targetPlatform("linux-x64") {
 	setJdkHome("stuff/jdk-15.0.2+7")
     	extraModulePaths.add("stuff/javafx-jmods-15.0.1")
-    }
+    }*/
     addExtraDependencies("kotlinx.coroutines.core.jvm")
     mergedModule {
 
@@ -98,11 +123,11 @@ tasks {
                 file("$buildDir/image/run-linux-x64/lib").mkdirs()
                 copy {
                     from("$projectDir/lib/baf2sql_adapter.dll", "$projectDir/lib/baf2sql_c.dll")
-                    into("$buildDir/image/run-windows-x64/lib")
+                    into("$buildDir/image/lib")
                 }
                 copy {
                     from("$projectDir/lib/libbaf2sql_adapter.so", "$projectDir/lib/libbaf2sql_c.so")
-                    into("$buildDir/image/run-linux-x64/lib")
+                    into("$buildDir/image/lib")
                 }
             }
         }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,36 @@
-rootProject.name = "kbaf2mzml"
+
 
 // We don't use that, impossible to build properly using gradle
 // Use the build_adapter.sh script on a linux machine
 //include(":baf2sql_adapter")
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+        maven("https://plugins.gradle.org/m2/")
+    }
+
+    plugins {
+        val versionsPluginVersion: String by settings
+        val kotlinVersion: String by settings
+        val jlinkVersion: String by settings
+        val detektVersion: String by settings
+        val ktlintVersion: String by settings
+        val kotlinterVersion: String by settings
+        val openjfxPluginVersion: String by settings
+
+        kotlin("jvm") version kotlinVersion
+        id("com.github.ben-manes.versions") version versionsPluginVersion
+        id("org.beryx.jlink") version jlinkVersion
+
+        id("io.gitlab.arturbosch.detekt") version detektVersion
+        id("org.jmailen.kotlinter") version kotlinterVersion
+        id("org.jlleitschuh.gradle.ktlint") version ktlintVersion
+
+        id("org.openjfx.javafxplugin") version openjfxPluginVersion
+    }
+}
+
+val projectName: String by settings
+rootProject.name = projectName
